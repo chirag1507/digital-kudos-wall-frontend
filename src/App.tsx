08da30cf120d2d1a91abfaad44b67f367a5a476e
chirag1507 <chirag@avestatechnologies.com>
2025-06-04 19:03:19 +0530

    Minor changes

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ function App(): JSX.Element {
     <div className="App">
       <header className="App-header">
         <h1>Digital Kudos Wall Platform</h1>
-        <p>Welcome to the Digital Kudos Wall - MVP Version</p>
+        <p>Welcome to the Digital Kudos Wall - MVP Version.</p>
         <p>A place to give and receive kudos within your team.</p>
       </header>
     </div>
