bf8fb2ff154ac4e8ada1cab84a43fbb9d2aee7dc
chirag1507 <chirag@avestatechnologies.com>
2025-07-18 17:15:56 +0530

    chore: add debug logging to RegisterUserUseCase

--- src/features/auth/application/use-cases/register-user/RegisterUserUseCase.ts
+++ src/features/auth/application/use-cases/register-user/RegisterUserUseCase.ts
@@ -5,6 +5,7 @@ export class RegisterUserUseCase {
   constructor(private readonly authService: AuthService) {}
 
   async execute(payload: RegisterUserPayload): Promise<User> {
+    console.log("RegisterUserUseCase", payload);
     return this.authService.registerUser(payload);
   }
 }
